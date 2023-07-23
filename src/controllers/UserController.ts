911067a6a523cea212ce49523fc9eb561b8edf6c
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-23 02:10:00 -0400

    Add: Creating a User

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
 import { Faculty } from "../models/Faculty";
-
+import { validationResult } from "express-validator";
+import { UserRequest } from "../types";
 export default class UserController {
     // Implement Controllers
     public static async getAll(req: Request, res: Response) {
@@ -11,5 +12,28 @@ export default class UserController {
         res.status(200).json({
             data: users
         });
+    }
+
+    public static async create(req: UserRequest, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty) return res.status(400).json({ errors });
+
+        try {
+            const { fullName, email, password, facultyId } = req.body;
+            await User.create({ fullName, email, password, facultyId });
+            res.status(200).json({
+                message: "User created successfully",
+                user: {
+                    fullName,
+                    email
+                }
+            });
+        }
+        catch (err) {
+            res.status(500).json({
+                message: "Server internal error",
+                errors: err.message
+            });
+        }
     }
 }
