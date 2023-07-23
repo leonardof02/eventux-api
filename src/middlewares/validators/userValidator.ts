1bb805575fc8b1fa59c0e1cbae23ffe7e8eda286
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-23 02:09:31 -0400

    Fix: user validation doesn't work (infinity request)

--- src/middlewares/validators/userValidator.ts
+++ src/middlewares/validators/userValidator.ts
@@ -1,11 +1,13 @@
 import { body } from "express-validator";
+import { Middleware } from "express-validator/src/base";
 
-function userValidators() {
+export function userValidator(): Middleware[] {
     return [
-      body('name').notEmpty().withMessage('El nombre es requerido.'),
-      body('email').isEmail().withMessage('El correo electrónico es inválido.'),
-      body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres.'),
+      body('name')
+        .notEmpty().withMessage('El nombre de usuario es requerido.')
+        .matches(/^[a-zA-Z\s]+$/).withMessage("El nombre solo puede contener letras y espacios"),
+      body("email").isEmail().withMessage('El correo electrónico es inválido.'),
+      body("password").isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres.'),
+      body("facultyId").isNumeric().withMessage("La facultad no existe")
     ];
-  }
-  
-  module.exports = userValidators;
+}
--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import UserController from "../controllers/UserController";
+import { userValidator } from "../middlewares/validators/userValidator";
 
 // Route name: /users
 const UserRouter = express.Router();
 
 // -- Configure routes
 UserRouter.get( "/", UserController.getAll );
+UserRouter.post( "/", userValidator(), UserController.create );
 
 export default UserRouter;
