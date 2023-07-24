f2c47cfe8b9741e0ec4fe5583defcc3102e453ac
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-24 00:34:11 -0400

    Add: Create Event controller

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import { User } from "../models/User";
 import { Event } from "../models/Event";
 import { EventRequest } from "../types";
+import { validationResult } from "express-validator";
 
 export default class EventController {
-
     // Implement Controllers
     public static async getAll(req: Request, res: Response) {
         const events = await Event.findAll({
@@ -15,7 +15,27 @@ export default class EventController {
         });
     }
 
-    public static async create( req: EventRequest, res: Response ) {
-        res.send( req.userId.toString() );
+    public static async create(req: EventRequest, res: Response) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors });
+
+        try {
+            const { name, description } = req.body;
+            const newEvent = await Event.create({
+                name,
+                description,
+                userId: req.userId
+            });
+            res.status(200).json({
+                message: "Event Created!",
+                event: newEvent.dataValues
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: "Server internal error",
+                errors: err.message
+            });
+        }
     }
 }
--- src/middlewares/validators/userValidator.ts
+++ src/middlewares/validators/userValidator.ts
@@ -3,11 +3,19 @@ import { Middleware } from "express-validator/src/base";
 
 export function userValidator(): Middleware[] {
     return [
-      body('fullName')
-        .notEmpty().withMessage('El nombre de usuario es requerido.')
-        .matches(/^[a-zA-Z\s]+$/).withMessage("El nombre solo puede contener letras y espacios"),
-      body("email").isEmail().withMessage('El correo electrónico es inválido.'),
-      body("password").isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres.'),
-      body("facultyId").isNumeric().withMessage("La facultad no existe")
+        body("fullName")
+            .notEmpty()
+            .withMessage("El nombre de usuario es requerido.")
+            .matches(/^[a-zA-Z\s]+$/)
+            .withMessage("El nombre solo puede contener letras y espacios"),
+        body("email")
+            .notEmpty()
+            .withMessage("El email es requerido")
+            .isEmail()
+            .withMessage("El correo electrónico es inválido."),
+        body("password")
+            .isLength({ min: 6 })
+            .withMessage("La contraseña debe tener al menos 6 caracteres."),
+        body("facultyId").isNumeric().withMessage("La facultad no existe")
     ];
 }
