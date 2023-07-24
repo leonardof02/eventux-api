ce22f23b8a9390df6ad3d3fa62c26692ebe5eef3
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-24 00:37:03 -0400

    Add: Event Validator

---
+++ src/middlewares/validators/eventValidator.ts
@@ -0,0 +1,13 @@
+import { body } from "express-validator";
+import { Middleware } from "express-validator/src/base";
+
+export function userValidator(): Middleware[] {
+    return [
+        body("name")
+            .notEmpty()
+            .withMessage("El nombre del evento es requerido.")
+            .matches(/^[a-zA-Z\s]+$/)
+            .withMessage("El nombre solo puede contener letras y espacios"),
+        body("description").isString().withMessage("Descripcion invalida")
+    ];
+}
